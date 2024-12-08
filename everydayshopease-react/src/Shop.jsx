61b0524e31f99c3a7663a6150f65bb145abd467d
Jangchaeyun <zzangsally@naver.com>
2024-12-08 19:44:03 +0900

    1208 Create HeroSection

--- everydayshopease-react/src/Shop.jsx
+++ everydayshopease-react/src/Shop.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 const Shop = () => {
   return (
     <>
       <Navigation />
+      <HeroSection />
     </>
   );
 };
