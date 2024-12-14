df49bc7f32cd0494b9469b2a4fe66400c5c9f568
Jangchaeyun <zzangsally@naver.com>
2024-12-14 17:40:55 +0900

    1214 Product

---
+++ everydayshopease-react/src/pages/ProductListPage/ProductCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import SvgFavorite from "../../components/common/SvgFavorite";
+
+const ProductCard = ({
+  title,
+  description,
+  price,
+  discount,
+  rating,
+  brand,
+  thumbnail,
+}) => {
+  return (
+    <div className="flex flex-col hover:scale-105 relative">
+      <img
+        className={`h-[320px] w-[310px]
+         border rounded-lg cursor-pointer object-cover block`}
+        src={thumbnail}
+        alt={title}
+      />
+
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col pt-2">
+          <p className="text-[16px] p-1">{title}</p>
+          {description && (
+            <p className="text-[12px] px-1 text-gray-600">{brand}</p>
+          )}
+        </div>
+        <div>
+          <p>{price.toLocaleString()}원</p>
+        </div>
+      </div>
+      <button
+        onClick={() => console.log("Click button")}
+        className="absolute top-0 right-0 pt-4 pr-4 cursor-pointer"
+      >
+        <SvgFavorite />
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- everydayshopease-react/src/pages/ProductListPage/ProductListPage.jsx
+++ everydayshopease-react/src/pages/ProductListPage/ProductListPage.jsx
@@ -5,20 +5,25 @@ import Categories from "../../components/Filters/Categories";
 import PriceFilter from "../../components/Filters/PriceFilter";
 import ColorFilter from "../../components/Filters/ColorFilter";
 import SizeFilter from "../../components/Filters/SizeFilter";
+import ProductCard from "./ProductCard";
 
 const categories = content.categories;
 
 const ProductListPage = ({ categoryType }) => {
   const categoryContent = useMemo(() => {
-    return categories?.find(
-      (category) => category.code === categoryType.toUpperCase()
-    );
+    return categories?.find((category) => category.code === categoryType);
   }, [categoryType]);
+
+  const productListItems = useMemo(() => {
+    return content?.products?.filter(
+      (product) => product?.category_id === categoryContent?.id
+    );
+  }, [categoryContent]);
 
   return (
     <div>
       <div className="flex">
-        <div className="w-[25%] p-[20px] border rounded-lg m-[20px]">
+        <div className="w-[20%] p-[10px] border rounded-lg m-[20px]">
           {/* Filters */}
           <div className="flex justify-between">
             <p className="text-[16px] text-gray-600">필터</p>
@@ -37,8 +42,13 @@ const ProductListPage = ({ categoryType }) => {
           <SizeFilter sizes={categoryContent?.meta_data?.sizes} />
         </div>
         <div className="p-[15px]">
-          {/* Products  */}
           <p className="text-black text-lg">{categoryContent?.description}</p>
+          {/* Products  */}
+          <div className="pt-4 grid grid-cols-1 lg:grid-cols-3 md:grid-col-2 gap-8 px-2">
+            {productListItems?.map((item, index) => (
+              <ProductCard key={index} {...item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
