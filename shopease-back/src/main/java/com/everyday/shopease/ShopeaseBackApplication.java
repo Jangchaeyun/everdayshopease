109ac9df04d8734ec84958d39773992185e00626
Jangchaeyun <zzangsally@naver.com>
2024-12-23 19:24:35 +0900

    1223 Initialize Spring Boot Project

---
+++ shopease-back/src/main/java/com/everyday/shopease/ShopeaseBackApplication.java
@@ -0,0 +1,13 @@
+package com.everyday.shopease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeaseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeaseBackApplication.class, args);
+    }
+
+}
---
+++ shopease-back/src/test/java/com/everyday/shopease/ShopeaseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyday.shopease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeaseBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
