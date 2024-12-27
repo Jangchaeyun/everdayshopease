6582338851d614098aa3ac96b54d194813895e4b
Jangchaeyun <zzangsally@naver.com>
2024-12-27 23:46:59 +0900

    1227 API

--- everydayshopease-react/src/Shop.jsx
+++ everydayshopease-react/src/Shop.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeroSection from "./components/HeroSection/HeroSection";
 import NewArrivals from "./components/Sections/NewArrivals";
 import content from "./data/content.json";
 import Category from "./components/Sections/Categories/Category";
 import Footer from "./components/Footer/Footer";
+import { fetchCategories } from "./api/fetchCategories";
 
 const Shop = () => {
+  useEffect(() => {
+    fetchCategories()
+      .then((res) => {
+        console.log("Categories ", res);
+      })
+      .catch((err) => {});
+  }, []);
   return (
     <>
       <HeroSection />
